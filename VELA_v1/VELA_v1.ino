40551fd5fc82e62362fb99b701b6d6cbf42e9d74
Juanjo <100855055+JeyJeysp@users.noreply.github.com>
2022-10-15 22:04:50 +0200

    v1.2
    Implemento la función 'comprobarMicro'. Sigo mañana: necesito la placa para seguir haciendo pruebas.

--- VELA_v1/VELA_v1.ino
+++ VELA_v1/VELA_v1.ino
@@ -5,10 +5,11 @@
 
 
 // --- Definiciones --- //
-#define intervalo 200
+#define intervalo 50
 #define un_segundo 1000
 #define UMBRAL_NTC 350
-#define UMBRAL_Micro 600
+#define UMBRAL_Micro1 600
+#define UMBRAL_Micro2 400
 
 
 // --- Variables globales --- //
@@ -17,8 +18,8 @@ unsigned long tiempo_ant;
 
 
 // --- Funciones --- //
-void comprobarNTC (int v, unsigned long t);
-void comprobarMicro (int v, unsigned long t);
+void comprobarNTC (int v, unsigned long t);		//Funcion que chequea los valores del NTC y activa el rele.
+void comprobarMicro (int v, unsigned long t);	//Funcion que chequea los valores del micro y desactiva el rele.
 
 
 // --- Setup y Loop --- //
@@ -36,81 +37,20 @@ void loop() {
 	if (!encendido){          //VELA APAGADA: Sondeamos valores del NTC.
 		int valor = analogRead(A0);
 		unsigned long tiempo = millis();
+    	//Serial.print("TEMP: ");
+   		//Serial.println(valor);		
 		
 		comprobarNTC(valor, tiempo);
-    //Serial.print("TEMP: ");
-    //Serial.println(valor);
-    
-		if (valor < 350){
-			if (!valor_ant){
-				valor_ant = valor;
-				tiempo_ant = tiempo;
-			} else if ((valor < 475) && (valor < valor_ant) && (tiempo > (tiempo_ant + intervalo))) {    //Hemos comprobado que el valor del NTC cae y es menor que el anterior en 0.1 segundos.
-				Serial.println("        ENCIENDO");
-				encendido = 1;
-				valor_ant = 0;
-				tiempo_ant = 0;
-		
-				digitalWrite(2, 1);
-			} else if (((valor < 200) && (valor > valor_ant)) || (tiempo > (tiempo_ant + un_segundo))){
-				valor_ant = 0;
-				tiempo_ant = 0;
-			}
-		}
 	} else {                //VELA ENCENDIDA: Sondeamos valores del micro.
 		int valor2 = analogRead(A1);
-    //Serial.print("MICRO: ");
-    Serial.println(valor2);
-    int tiempo = millis();
-		  
-		if (valor2 > 700){
-      if (!tiempo_ant){
-        tiempo_ant = tiempo;
-      } else if ((valor2 > 700) && (tiempo > (tiempo_ant + intervalo*2))) {    //Hemos comprobado que el valor del NTC cae y es menor que el anterior en 0.1 segundos.
-        Serial.println("        APAGO");
-        encendido = 0;
-        tiempo_ant = 0;
-    
-        digitalWrite(2, 0);
-      } else if (tiempo > (tiempo_ant + un_segundo*2)){
-        tiempo_ant = 0;
-      }
-		
-		/*int umbral = 650;
-		if (valor2 > umbral){
-			int i = 0;
-			tiempo_ant = millis();
-			
-			while (i < 10){
-				if (millis() > (tiempo_ant + intervalo*i)){
-					i++;
-          valor2 = analogRead(A1);
-          //Serial.print("MICRO DD: ");
-          Serial.println(valor2);
-          
-					if (valor2 > umbral)
-         {
-						num_umbral++;
-           umbral-=50;
-         }
-				}
-			}
-			
-			if (num_umbral > 2){               //Comprobamos si sobrepasamos el umbral al menos 6 veces.
-				Serial.println("        APAGO");
-				
-				encendido = 0;
-				num_umbral = 0;
-				tiempo_ant = 0;
-				
-				digitalWrite(2, 0);
-			} else {
-				num_umbral = 0;
-				tiempo_ant = 0;
-			}*/
-		}
+		unsigned long tiempo2 = millis();
+    	//Serial.print("MICRO: ");
+    	//Serial.println(valor2);
+
+		comprobarMicro(valor2, tiempo2);
 	}
 }
+
 
 
 void comprobarNTC (int v, unsigned long t){
@@ -118,7 +58,7 @@ void comprobarNTC (int v, unsigned long t){
 		if (!valor_ant){
 			valor_ant = v;
 			tiempo_ant = t;
-		} else if ((valor < UMBRAL_NTC) && (valor < valor_ant) && (tiempo > (tiempo_ant + intervalo))) {    //Hemos comprobado que el valor del NTC cae y es menor que el anterior en 0.1 segundos.
+		} else if ((valor < UMBRAL_NTC) && (valor < valor_ant) && (tiempo > (tiempo_ant + intervalo))){    //Hemos comprobado que el valor del NTC cae y es menor que el anterior en 0.1 segundos.
 			//Serial.println("        ENCIENDO");
 			encendido = 1;
 			valor_ant = 0;
@@ -130,4 +70,56 @@ void comprobarNTC (int v, unsigned long t){
 			tiempo_ant = 0;
 		}
 	}
+}
+
+
+void comprobarMicro (int v, unsigned long t){
+	if ((v > UMBRAL_Micro1) || (v < UMBRAL_Micro2)){
+		if (!tiempo_ant){
+			tiempo_ant = t;
+		} else if (((v > UMBRAL_Micro1) || (v < UMBRAL_Micro2)) && (t > (tiempo_ant + intervalo))){
+			//Serial.println("        APAGO");
+			encendido = 0;
+			tiempo_ant = 0;
+
+			digitalWrite(2, 0);
+		} else if (t > (tiempo_ant + un_segundo)){
+			tiempo_ant = 0;
+		}
+	}
+	
+	
+	/*int umbral = 650;
+	if (valor2 > umbral){
+		int i = 0;
+		tiempo_ant = millis();
+		
+		while (i < 10){
+			if (millis() > (tiempo_ant + intervalo*i)){
+				i++;
+            	valor2 = analogRead(A1);
+            	//Serial.print("MICRO DD: ");
+            	Serial.println(valor2);
+          
+				if (valor2 > umbral)
+            	{
+					num_umbral++;
+                	umbral-=50;
+            	}
+			}
+		}
+			
+		if (num_umbral > 2){               //Comprobamos si sobrepasamos el umbral al menos 6 veces.
+			Serial.println("        APAGO");
+				
+			encendido = 0;
+			num_umbral = 0;
+			tiempo_ant = 0;
+				
+			digitalWrite(2, 0);
+		} else {
+			num_umbral = 0;
+			tiempo_ant = 0;
+		}
+	}*/
 }
