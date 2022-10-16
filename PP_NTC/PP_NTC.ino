cac7e1b92594ab25a0cbd25b44cbc26af3dae72b
juanjong93 <100855055+juanjong93@users.noreply.github.com>
2022-10-16 13:31:10 +0200

    V1_2
    Correcion de errores y ajuste de umbrales

--- PP_NTC/PP_NTC.ino
+++ PP_NTC/PP_NTC.ino
@@ -5,9 +5,9 @@ void setup() {
 void loop() {
   //double lectura = analogRead(A0);
   //Serial.print("NTC en A0: ");
-  Serial.println(analogRead(A0));
+  //Serial.println(analogRead(A0));
   //Serial.print("Micro en A1: ");
-  //Serial.println(analogRead(A1));
+  Serial.println(analogRead(A1));
   //Serial.println();
 
   delay(10);
--- VELA_v1/VELA_v1.ino
+++ VELA_v1/VELA_v1.ino
@@ -7,9 +7,8 @@
 // --- Definiciones --- //
 #define intervalo 50
 #define un_segundo 1000
-#define UMBRAL_NTC 350
-#define UMBRAL_Micro1 600
-#define UMBRAL_Micro2 400
+#define UMBRAL_NTC 400
+#define UMBRAL_Micro 650
 
 
 // --- Variables globales --- //
@@ -37,15 +36,15 @@ void loop() {
 	if (!encendido){          //VELA APAGADA: Sondeamos valores del NTC.
 		int valor = analogRead(A0);
 		unsigned long tiempo = millis();
-    	//Serial.print("TEMP: ");
-   		//Serial.println(valor);		
+    //Serial.print("TEMP: ");
+   	//Serial.println(valor);		
 		
 		comprobarNTC(valor, tiempo);
 	} else {                //VELA ENCENDIDA: Sondeamos valores del micro.
 		int valor2 = analogRead(A1);
 		unsigned long tiempo2 = millis();
-    	//Serial.print("MICRO: ");
-    	//Serial.println(valor2);
+    //Serial.print("MICRO: ");
+    //Serial.println(valor2);
 
 		comprobarMicro(valor2, tiempo2);
 	}
@@ -54,18 +53,18 @@ void loop() {
 
 
 void comprobarNTC (int v, unsigned long t){
-	if (valor < UMBRAL_NTC){
+	if (v < UMBRAL_NTC){
 		if (!valor_ant){
 			valor_ant = v;
 			tiempo_ant = t;
-		} else if ((valor < UMBRAL_NTC) && (valor < valor_ant) && (tiempo > (tiempo_ant + intervalo))){    //Hemos comprobado que el valor del NTC cae y es menor que el anterior en 0.1 segundos.
+		} else if ((v < UMBRAL_NTC) && (v < valor_ant-10) && (t > (tiempo_ant + intervalo))){    //Hemos comprobado que el valor del NTC cae y es menor que el anterior en 0.1 segundos.
 			//Serial.println("        ENCIENDO");
 			encendido = 1;
 			valor_ant = 0;
 			tiempo_ant = 0;
 		
 			digitalWrite(2, 1);
-		} else if (((valor < UMBRAL_NTC) && (valor > valor_ant)) || (tiempo > (tiempo_ant + un_segundo))){
+		} else if (((v < UMBRAL_NTC) && (v > valor_ant)) || (t > (tiempo_ant + un_segundo/2))){
 			valor_ant = 0;
 			tiempo_ant = 0;
 		}
@@ -74,52 +73,23 @@ void comprobarNTC (int v, unsigned long t){
 
 
 void comprobarMicro (int v, unsigned long t){
-	if ((v > UMBRAL_Micro1) || (v < UMBRAL_Micro2)){
-		if (!tiempo_ant){
-			tiempo_ant = t;
-		} else if (((v > UMBRAL_Micro1) || (v < UMBRAL_Micro2)) && (t > (tiempo_ant + intervalo))){
-			//Serial.println("        APAGO");
-			encendido = 0;
-			tiempo_ant = 0;
+	if (v > UMBRAL_Micro){
+    if (!tiempo_ant){
+      tiempo_ant = t;
+    } else if ((v > UMBRAL_Micro) && (t > (tiempo_ant + intervalo*4))){
+      if(num_umbral < 3)
+        num_umbral++;
+      else{
+        //Serial.println("        APAGO");
+        encendido = 0;
+        tiempo_ant = 0;
+        num_umbral = 0;
 
-			digitalWrite(2, 0);
-		} else if (t > (tiempo_ant + un_segundo)){
-			tiempo_ant = 0;
-		}
-	}
-	
-	
-	/*int umbral = 650;
-	if (valor2 > umbral){
-		int i = 0;
-		tiempo_ant = millis();
-		
-		while (i < 10){
-			if (millis() > (tiempo_ant + intervalo*i)){
-				i++;
-            	valor2 = analogRead(A1);
-            	//Serial.print("MICRO DD: ");
-            	Serial.println(valor2);
-          
-				if (valor2 > umbral)
-            	{
-					num_umbral++;
-                	umbral-=50;
-            	}
-			}
-		}
-			
-		if (num_umbral > 2){               //Comprobamos si sobrepasamos el umbral al menos 6 veces.
-			Serial.println("        APAGO");
-				
-			encendido = 0;
-			num_umbral = 0;
-			tiempo_ant = 0;
-				
-			digitalWrite(2, 0);
-		} else {
-			num_umbral = 0;
-			tiempo_ant = 0;
-		}
-	}*/
+        digitalWrite(2, 0);        
+      }
+    } else if (t > (tiempo_ant + un_segundo/2)){
+      tiempo_ant = 0;
+      num_umbral = 0;
+    }
+  }
 }
