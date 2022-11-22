0cc125b859dcf47a4750562befc6571bbdf507b2
Antoniopc12 <antoniopcas12@gmail.com>
2022-11-23 00:40:21 +0100

    Version Pistola disparo único

--- Pistol/Pistol.ino
+++ Pistol/Pistol.ino
@@ -4,10 +4,13 @@
   int pressed=0;
   int timeout=0;
   unsigned long timepass1=0, timepass2=0;
-  int municion=50;
+  int ammunition=10 ;
+  int shoot=0;
+  int no_ammo=0;
 void setup(){
 
   pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
   pinMode(7,INPUT_PULLUP);
   Serial.begin(9600);
 }
@@ -20,28 +23,22 @@ void setup(){
     digitalWrite(6,LOW);
   }
 }*/
-void loop() {
+/*void loop() {
   unsigned long tiempo=millis();
   Serial.print("Timepass1,timepass2: ");
   Serial.print(timepass1);
   Serial.print(" ");   
   Serial.println(timepass2);
-  if(timeout == 1 && tiempo-timepass2>5000)
-  {
-    timeout=0;
-  } else if(timeout==0)
+  if(timeout==0)
   {
     if(digitalRead(7) == 0 && pressed == 0)
     {
       pressed=1;
-      
       timepass1=tiempo;
-      
       digitalWrite(6,HIGH);
     }
     else if(pressed == 1)
     {
-      
       if(digitalRead(7) == 1)
       {
         digitalWrite(6,LOW);
@@ -55,6 +52,47 @@ void loop() {
         pressed=0;
       }
     }
-  }
+  } else if(timeout == 1 && tiempo-timepass2>5000)
+  {
+    timeout=0;
+  } 
   delay(50);
+}*/
+void loop()
+{
+  unsigned long tiempo=millis();
+    Serial.print("ammunition: ");
+    Serial.println(ammunition);
+   if(digitalRead(7) == 0 && shoot == 0 && timeout == 0 && ammunition>0)
+  {
+    Serial.println("2");
+    digitalWrite(6,HIGH);
+    timepass1=tiempo;
+    shoot=1;
+    ammunition--;
+    timeout = 1;
+    if(ammunition == 0)
+    {
+      no_ammo = 1;
+      timepass2=tiempo;
+      digitalWrite(5,HIGH);
+    }
+  }else if(digitalRead(7) == 1 && shoot == 1)
+  {
+    Serial.println("3");
+    shoot = 0;
+  } else if(tiempo-timepass1>50 && timeout == 1)
+  {
+      
+    Serial.println("1");
+    digitalWrite(6,LOW);
+    timeout = 0;
+  }else if(tiempo-timepass2>10000 && no_ammo == 1)
+  {
+    digitalWrite(5,LOW);
+    ammunition=10;
+    timeout = 0;
+    shoot = 0;
+    no_ammo = 0;
+  }
 }
