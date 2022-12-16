01d843813b96af0fe6cca9f2b27b7fd5e26a3655
Antoniopc12 <antoniopcas12@gmail.com>
2022-12-16 20:11:25 +0100

    Pistola Laser V1.0, con parpadeo en indicador de munición

--- Pistol/Pistol.ino
+++ Pistol/Pistol.ino
@@ -1,150 +1,81 @@
-  /* This example demonstrates use of the internal temperature sensor.
+    /* This example demonstrates use of the internal temperature sensor.
 |  Be sure to read the datasheet, particularly the part about how
 \  the sensor is basically uncalibrated. */
-#define P1 2 
-#define P2 3
-#define P3 4
-#define P4 5
-  int pressed=0;
-  int timeout=0;
-  unsigned long timepass1=0, timepass2=0, antirrebotes = 0;
-  int ammunition=10;
-  int shoot=0;
-  int no_ammo=0;
-  int modo=-1;
-  
+#define Ray 6 
+#define Led 5
+#define Trigger 4
+#define buzzer 7
+int pressed=0;
+int time_out=0;
+unsigned long time_pass1=0, time_pass2=0, antibounce = 0, timeled=0;
+int ammunition=20;
+int shoot=0;
+int no_ammo=0;
+int led_blink=0;
+int led=0;
 void setup(){
 
-  pinMode(P1,OUTPUT);
-  pinMode(P2,OUTPUT);
-  pinMode(P3,INPUT_PULLUP);
+  pinMode(Ray,OUTPUT);
+  pinMode(buzzer,OUTPUT);
+  pinMode(Led,OUTPUT);
+  pinMode(Trigger,INPUT_PULLUP);
   Serial.begin(9600);
 }
-/*void loop()
-{
-  if(digitalRead(7) == 1)
-  {
-    digitalWrite(6,HIGH);
-  } else {
-    digitalWrite(6,LOW);
-  }
-}*/
-/*void loop() {
-  unsigned long tiempo=millis();
-  Serial.print("Timepass1,timepass2: ");
-  Serial.print(timepass1);
-  Serial.print(" ");   
-  Serial.println(timepass2);
-  if(timeout==0)
-  {
-    if(digitalRead(7) == 0 && pressed == 0)
-    {
-      pressed=1;
-      timepass1=tiempo;
-      digitalWrite(6,HIGH);
-    }
-    else if(pressed == 1)
-    {
-      if(digitalRead(7) == 1)
-      {
-        digitalWrite(6,LOW);
-        pressed=0;
-      }
-       else if(tiempo-timepass1>2000)
-      {
-        timeout=1;
-        digitalWrite(6,LOW);
-        timepass2=tiempo;
-        pressed=0;
-      }
-    }
-  } else if(timeout == 1 && tiempo-timepass2>5000)
-  {
-    timeout=0;
-  } 
-  delay(50);
-}*/
+
 void loop()
 {
-  unsigned long tiempo=millis();
+  unsigned long time_=millis();
   Serial.print("ammunition: ");
   Serial.println(ammunition);
-  /*modo=digitalRead(P4);
-  if(modo == 1)
-  {*/
-    if(digitalRead(P3) == 0 && shoot == 0 && tiempo - antirrebotes > 200 && timeout == 0 && ammunition>0)
+  //modo=digitalRead(P4);
+  //if(modo == 0)
+    if(digitalRead(Trigger) == 0 && shoot == 0 && time_ - antibounce > 200 && time_out == 0 && ammunition>0)
     {
-      digitalWrite(P1,HIGH);
-      timepass1=tiempo;
+      digitalWrite(Ray,HIGH);
+      time_pass1=time_;
       shoot=1;
       ammunition--;
-      timeout = 1;
+      time_out = 1;
+      for(int x = 4000; x > 1000; x--)
+      {
+        tone(buzzer,x);
+      };
+      noTone(buzzer);
       if(ammunition == 0)
       {
         no_ammo = 1;
-        timepass2=tiempo;
-        digitalWrite(P2,HIGH);
+        time_pass2=time_;
+        digitalWrite(Led,HIGH);
+        led_blink = 1;
       }
-    }else if(digitalRead(P3) == 1 && shoot == 1)
+    }else if(digitalRead(Trigger) == 1 && shoot == 1)
     {
       shoot = 0;
 
-    }   else if(tiempo-timepass1>50 && timeout == 1)
+    }   else if(time_-time_pass1>50 && time_out == 1)
     {  
-      digitalWrite(P1,LOW);
-      antirrebotes = tiempo;
-      timeout = 0;
-    }else if(tiempo-timepass2>10000 && no_ammo == 1)
+      digitalWrite(Ray,LOW);
+      antibounce = time_;
+      time_out = 0;
+    }else if(time_-time_pass2>10000 && no_ammo == 1)
     {
-      digitalWrite(P2,LOW);
-      ammunition=10;
-      timeout = 0;
+      digitalWrite(Led,LOW);
+      ammunition=20;
+      time_out = 0;
       shoot = 0;
       no_ammo = 0;
-    }
-  } /*else 
-  {
-    
-    if(digitalRead(P3) == 0 && shoot == 0 && tiempo-timepass1 > 100 && timeout == 0 && ammunition>0)
+      led_blink=0;
+    } else if(led_blink == 1 && time_ - timeled > 500)
     {
-      Serial.println("AUTO 1");
-      digitalWrite(P1,HIGH);
-      timepass1=tiempo;
-      shoot=1;
-      ammunition--;
-      //timeout = 1;
-      if(ammunition == 0)
+      timeled = time_;
+      if(led == 0)
       {
-        no_ammo = 1;
-        timepass2=tiempo;
-        digitalWrite(P2,HIGH);
-      }
-    } else if(digitalRead(P3) == 0 && tiempo-timepass1 > 50 && shoot == 1 && timeout == 0 && ammunition>0)
-    {
-      Serial.println("AUTO 2");
-      digitalWrite(P1,LOW);
-      timepass1=tiempo;
-      shoot=0;
-      ammunition--;
-      //timeout = 1;
-      if(ammunition == 0)
+        digitalWrite(Led,0);
+        led+=1;
+      } else
       {
-        no_ammo = 1;
-        timepass2=tiempo;
-        digitalWrite(P2,HIGH);
+        digitalWrite(Led,1);
+        led-=1;
       }
     }
-    else if(digitalRead(P3) == 1)
-    {
-      digitalWrite(P1,LOW);
-    }
-    else if(tiempo-timepass2>10000 && no_ammo == 1)
-    {
-      digitalWrite(P2,LOW);
-      ammunition=10;
-      timeout = 0;
-      shoot = 0;
-      no_ammo = 0;
-    }
-}
-}*/
+  } 
